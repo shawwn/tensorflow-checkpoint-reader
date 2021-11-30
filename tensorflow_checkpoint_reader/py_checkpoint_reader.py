73666ddd7a5bf4c2dbc97362c9c0f86e8f5e637c
Shawn Presser <shawnpresser@gmail.com>
2021-11-30 16:48:44 -0600

    Decode as utf-8 strings

--- tensorflow_checkpoint_reader/py_checkpoint_reader.py
+++ tensorflow_checkpoint_reader/py_checkpoint_reader.py
@@ -7,10 +7,10 @@ class NewCheckpointReader:
     self._reader = bundle_reader.BundleReader(env.Env.default(), filename)
 
   def get_variable_to_shape_map(self):
-    return {name: arr.shape for name, arr in self._reader._tensors}
+    return {name.decode('utf-8'): arr.shape for name, arr in self._reader._tensors}
 
   def get_variable_to_dtype_map(self):
-    return {name: arr.dtype for name, arr in self._reader._tensors}
+    return {name.decode('utf-8'): arr.dtype for name, arr in self._reader._tensors}
 
   def get_tensor(self, name):
     name = core.string_view(name).bytes()
