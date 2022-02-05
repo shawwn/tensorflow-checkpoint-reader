87163222f96bd9913b21b9d4c56a2855dddfb195
Shawn Presser <shawnpresser@gmail.com>
2022-02-04 23:25:30 -0600

    test: add stub unit tests

--- tests/tensorflow_checkpoint_reader_test.py
+++ tests/tensorflow_checkpoint_reader_test.py
@@ -1,2 +1,9 @@
 import unittest
 from tensorflow_checkpoint_reader.pb.tensorflow.core.protobuf import tensor_bundle_pb2
+
+class TensorflowCheckpointReaderTestCase(unittest.TestCase):
+  def test_basic(self):
+    self.assertEqual(1, 1)
+
+if __name__ == '__main__':
+  unittest.main()
